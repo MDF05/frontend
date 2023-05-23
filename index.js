7ea9b9f03afd5b501134e5905ac948de37ddd4d2
Muhammad Dava Fahreza <mdavafahreza05@gmail.com>
2023-05-23 12:09:30 +0700

    baru lagi

--- index.js
+++ index.js
@@ -1,16 +1,16 @@
-fetch('https://server-cam.vercel.app/', {
-        method: 'POST',
-        mode: 'no-cors',
-        headers: {
-            'Content-Type': 'application/json',
-        },
-        body: JSON.stringify({ video: 'value' })
-    })
-    .then(response => {
-        // Lakukan sesuatu dengan respons
-        console.log(response.body)
-    })
-    .catch(error => {
-        // Tangani error
-        console.log(error)
-    });
+// fetch('https://server-cam.vercel.app/', {
+//         method: 'POST',
+//         mode: 'no-cors',
+//         headers: {
+//             'Content-Type': 'application/json',
+//         },
+//         body: JSON.stringify({ video: 'value' })
+//     })
+//     .then(response => {
+//         // Lakukan sesuatu dengan respons
+//         console.log(response.body)
+//     })
+//     .catch(error => {
+//         // Tangani error
+//         console.log(error)
+//     });
