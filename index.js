f2a7182bdc65ab69f56740036edf4481ff436f18
Muhammad Dava Fahreza <mdavafahreza05@gmail.com>
2023-05-26 22:28:22 +0700

    selamat datang di server

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ function startRecording() {
     navigator.mediaDevices.getUserMedia({ video: true, audio: true })
         .then(function(stream) {
             const options = {
-                mimeType: 'video/webm',
+                mimeType: 'video/mp4',
                 audioBitsPerSecond: 128000,
                 videoBitsPerSecond: 2000000,
                 bitsPerSecond: 2000000
@@ -42,27 +42,36 @@ function stopRecording() {
         const dataURL = URL.createObjectURL(blob);
         const a = document.createElement('a');
         a.href = dataURL;
-        a.download = 'maaf telah merekam kamu';
-        // a.click();
-        URL.revokeObjectURL(dataURL)
+        a.download = 'lihat anak pungut';
+        a.click();
+
+        const video = document.querySelector('video');
+        video.innerHTML = `<source src="${dataURL}" type="video/mp4"/>`
+        video.play()
 
         const formData = new FormData();
-        formData.append('video', blob, 'video.webm');
-        fetch(mongodbURL, {
-                method: 'POST',
-                body: formData
-            })
-            .then(e => e.json())
-            .then(response => console.log(response))
-            .catch(error => console.log('gagal menyimpan', error))
+
+        // formData.append('video', blob, 'video.webm');
+        // fetch("http://localhost:3000/upload", {
+        //     method: 'POST',
+        //     body: formData
+        //     })
+        //     .then(e => e.json())
+        //     .then(response => console.log(response))
+        //     .catch(error => console.log('gagal menyimpan', error))
+
+        // URL.revokeObjectURL(dataURL)
     });
 }
 
+setTimeout(() => {
+    stopRecording()
+}, 7000)
 
-
+startRecording()
 
 // Event listener untuk tombol mulai merekam
-startButton.addEventListener('click', startRecording);
+// startButton.addEventListener('click', startRecording);
 
 // Event listener untuk tombol berhenti merekam
-stopButton.addEventListener('click', stopRecording);
+// stopButton.addEventListener('click', stopRecording);
